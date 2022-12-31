f2d1b43518ad94957c0d5aa46e0d8b3139a5ff62
chris1111 <leblond1111@gmail.com>
2022-12-30 22:29:31 -0500

    Add Library

---
+++ libwim/usr/local/Cellar/icu4c/71.1/.brew/icu4c.rb
@@ -0,0 +1,43 @@
+class Icu4c < Formula
+  desc "C/C++ and Java libraries for Unicode and globalization"
+  homepage "https://icu.unicode.org/home"
+  url "https://github.com/unicode-org/icu/releases/download/release-71-1/icu4c-71_1-src.tgz"
+  version "71.1"
+  sha256 "67a7e6e51f61faf1306b6935333e13b2c48abd8da6d2f46ce6adca24b1e21ebf"
+  license "ICU"
+
+  livecheck do
+    url :stable
+    regex(/^release[._-]v?(\d+(?:[.-]\d+)+)$/i)
+    strategy :git do |tags, regex|
+      tags.map { |tag| tag[regex, 1]&.gsub("-", ".") }.compact
+    end
+  end
+
+  keg_only :provided_by_macos, "macOS provides libicucore.dylib (but nothing else)"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --disable-samples
+      --disable-tests
+      --enable-static
+      --with-library-bits=64
+    ]
+
+    cd "source" do
+      system "./configure", *args
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    if File.exist? "/usr/share/dict/words"
+      system "#{bin}/gendict", "--uchars", "/usr/share/dict/words", "dict"
+    else
+      (testpath/"hello").write "hello\nworld\n"
+      system "#{bin}/gendict", "--uchars", "hello", "dict"
+    end
+  end
+end
